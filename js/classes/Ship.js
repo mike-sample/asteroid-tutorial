6304a1d295b5cf53a751c08e0c865119484a0730
Michael Sample <mikebsample@gmail.com>
2019-06-28 15:12:26 -0700

    beginning ship controls

--- js/classes/Ship.js
+++ js/classes/Ship.js
@@ -1,6 +1,8 @@
 import MovingObject from '/js/classes/MovingObject.js'
 import Canvas from '/js/utility/Canvas.js'
 import Vec2 from '/js/classes/Vec2.js';
+import key from 'keymaster'
+
 const {PI:pi, cos, sin} = Math
 const SHIP_RADIUS_BODY = 10
 const SHIP_RADIUS_HEAD = 5
@@ -25,5 +27,13 @@ export default class Ship extends MovingObject {
             radius: SHIP_RADIUS_HEAD,
             color: this.color
         });
+    }
+    move() {
+        if(key.isPressed("left")) {
+            this.direction -= .1
+        }
+        if(key.isPressed("right")) {
+            this.direction += .1
+        }
     }
 }
